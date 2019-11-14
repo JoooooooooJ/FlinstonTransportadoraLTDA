9e5e6ae3c9eeaf56fe8b152294eba6c70a7f27aa
JooJ <50976403+JoooooooooJ@users.noreply.github.com>
2019-11-13 21:37:54 -0300

    AddressDao ADD pronto

--- src/Driver/Driver.java
+++ src/Driver/Driver.java
@@ -16,6 +16,87 @@ public class Driver {
     private Calendar expiration;
     private boolean status;
     private Address address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public long getRG() {
+        return RG;
+    }
+
+    public void setRG(long RG) {
+        this.RG = RG;
+    }
+
+    public long getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(long CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getCNHnum() {
+        return CNHnum;
+    }
+
+    public void setCNHnum(long CNHnum) {
+        this.CNHnum = CNHnum;
+    }
+
+    public char getCNHtype() {
+        return CNHtype;
+    }
+
+    public void setCNHtype(char CNHtype) {
+        this.CNHtype = CNHtype;
+    }
+
+    public Calendar getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Calendar expiration) {
+        this.expiration = expiration;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }    
+    
     
             
     
--- src/util/AddressDao.java
+++ src/util/AddressDao.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package util;
 
-/**
- *
- * @author Usuario
- */
-public class AddressDao {
+import DAO.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collection;
+
+public class AddressDao implements DAO<Address>{
+
+    @Override
+    public void add(Object obj) {
+        
+        try{
+            Connection con = new ConnectionBuilder().getConnection();
+            Address address = (Address) obj;
+            String sql = "insert into endereco(id,rua,numero,cidade,cep,UF)"
+                      +  "values(?,?,?,?,?,?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setLong(1, address.getId());
+            stmt.setString(2,address.getStreet());
+            stmt.setInt(3,address.getNumber());
+            stmt.setString(4,address.getCity());
+            stmt.setString(5,address.getCEP());
+            stmt.setString(6,address.getUF());
+            
+            stmt.execute();
+            stmt.close();
+            
+            con.close();
+            
+        }catch(SQLException e){
+            
+            throw new RuntimeException(e);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void update(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object read(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Address> getList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
